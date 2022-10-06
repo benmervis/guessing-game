527d7ad22e996c66dff40181062b7f0ed548f0a9
merve üstünel <94653080+benmervis@users.noreply.github.com>
2022-10-06 18:33:47 +0300

    added file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+
+Random random = new Random();
+
+int winNum = random.Next(0, 100);
+
+bool win = false;
+
+do
+{
+    Console.WriteLine("Guess a number in between 0 and 100: ");
+    string s = Console.ReadLine();
+
+    int i = int.Parse(s);
+
+    if (i > winNum)
+        Console.WriteLine("too high!! guess lower!");
+    else if (i < winNum)
+        Console.WriteLine("too low!! guess higher!");
+    else if (i == winNum)
+    {
+        Console.WriteLine("YOU WIN!");
+        win = true;
+    }
+
+} while (win == false);
